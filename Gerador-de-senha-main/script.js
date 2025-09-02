c12a74dbd8bb04c057ad870a6ca8a5780d3f0a7c
alex-santelli <alex.santelli.junior@escola.pr.gov.br>
2025-09-02 11:20:44 -0300

    Add files via upload

---
+++ Gerador-de-senha-main/script.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-principal");
+
+
+
+const perguntas = [
+    {
+    enunciado  "pergunta 1",
+    alternativas:[
+        "Alternativa 1",
+        "Alternativa 2",
+
+    ]
+}
+]
